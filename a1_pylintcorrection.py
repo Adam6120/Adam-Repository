bcdb74054a6b6e956f6a693034a6bcc84a3d8a1e
Adam6120 <adam.hussain.2022@uni.strath.ac.uk>
2026-02-03 17:39:43 +0000

    Updated python code with further pylint improvements

--- a1_pylintcorrection.py
+++ a1_pylintcorrection.py
@@ -3,24 +3,23 @@
 """
 Code for executing an integral which yields the value of pi, using the mpi module for parallelism.
 """
-from mpi4py import MPI
 import time  #Time module for benchmarking
-#import math #module for trying to fixe the output fluctuation for higher cores
+from mpi4py import MPI
 
-comm = MPI.COMM_WORLD   #Initiation of the communication between processors
-nproc = comm.Get_size() #Number of processes (4)
-rank = comm.Get_rank()  #Assign rank to processors (4 ranks: Rank 0,1,2,3)
-N = 100000000           #100,000,000 Points
-DELTA = 1.0 / N         #Step Width of 1/100000000 from 0 to 1.
-INTEGRAL_PIECE = 0.0     #Initial value for rank's contribution
-N_SPLIT = N // nproc #Splitting work among processors
+comm = MPI.COMM_WORLD   # Initiation of the communication between processors
+nproc = comm.Get_size() # Number of processes (4)
+rank = comm.Get_rank()  # Assign rank to processors (4 ranks: Rank 0,1,2,3)
+N = 100000000           # 100,000,000 Points
+DELTA = 1.0 / N         # Step Width of 1/100000000 from 0 to 1.
+INTEGRAL_PIECE = 0.0     # Initial value for rank's contribution
+N_SPLIT = N // nproc # Splitting work among processors
 
-if rank < nproc -1:        #Ensures the final rank gets the final split.
+if rank < nproc -1:        # Ensures the final rank gets the final split.
     start = rank * N_SPLIT #Each processor gets N/NSplit
-    end = start + N_SPLIT #For uneven splits of N
+    END = start + N_SPLIT #For uneven splits of N
 else:
     start = rank * N_SPLIT
-    end = N
+    END = N
 
 def integrand(x):
     """
@@ -32,7 +31,7 @@ comm.Barrier() #Synchronises ranks, required for accurate timing
 if rank == 0:
     starttime = time.time() #START timing here
 
-for i in range(start, end):
+for i in range(start, END):
     y = (i + 0.5) * DELTA
     INTEGRAL_PIECE += integrand(y) * DELTA#Function loops, final value sent to integral piece
 
